b3977411a0a9f12a044f43b235fab49eae621cf5
Ian Groff <ian@iangroff.com>
2021-08-26 17:01:56 -0500

    Swizzling this so we can capture requests for future review

--- echo.py
+++ echo.py
@@ -7,33 +7,43 @@ import os
 
 BaseHTTPServer.MessageClass = Message
 
+responseBuffer = []
+responseBufferLength = os.environ.get('RESPONSE_BUFFER_LENGTH', 10)
+
 class EchoHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     # a method for handling our any requests given, simply echo's back
     # the content of the request including any headers as a json object
     def handle_request(self):
-        resp_data = dict(clientAddress=self.client_address[0],
-            method=self.command,
-            uri=self.path,
-            httpVersion=self.request_version,
-            requestHeaders={key:self.headers[key] for key in self.headers.keys()}
-        )
+        if self.path == "/____show":
+            response_body = json.dumps(responseBuffer)
+        else:
+            resp_data = dict(clientAddress=self.client_address[0],
+                method=self.command,
+                uri=self.path,
+                httpVersion=self.request_version,
+                requestHeaders={key:self.headers[key] for key in self.headers.keys()}
+            )
+            # check for a request body, and if we have it we'll need to know how long
+            # it is to read it
+            request_length = int(resp_data['requestHeaders'].get('content-length', 0))
+            if request_length:
+                resp_data['body'] = self.rfile.read(request_length)
+            # pick out our app specific config from the environment
+            environment_config = {}
+            for n in os.environ:
+                if not n.startswith("APP_CONFIG_"):
+                    continue
+            environment_config[n] = os.environ[n]
+            resp_data["environment_config"] = environment_config
+            responseBuffer.append(resp_data)
+            # if we have more entries than our 'limit' just remove the oldest
+            if len(responseBuffer) > responseBufferLength:
+                responseBuffer.pop(0)
+            response_body = json.dumps(resp_data)
         # everything is always good
         self.send_response(200, '')
         # we'll be returning json
         self.send_header('content-type', 'application/json')
-        # check for a request body, and if we have it we'll need to know how long
-        # it is to read it
-        request_length = int(resp_data['requestHeaders'].get('content-length', 0))
-        if request_length:
-            resp_data['body'] = self.rfile.read(request_length)
-        # pick out our app specific config from the environment
-        environment_config = {}
-        for n in os.environ:
-          if not n.startswith("APP_CONFIG_"):
-            continue
-          environment_config[n] = os.environ[n]
-        resp_data["environment_config"] = environment_config
-        response_body = json.dumps(resp_data)
         # we're sending the json object as the body, so length of the json object
         self.send_header('content-length', len(response_body))
         # first we end our header section as per HTTP requirements (\n), then
